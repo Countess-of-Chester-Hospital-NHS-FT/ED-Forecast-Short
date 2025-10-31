c85f59f04453cd3a0d2a4cc8fe390f1351118dcc
helenajr <helenajrobinson@gmail.com>
2025-10-31 14:51:24 +0000

    Added baseline forecast and plotted accuracy

--- baseline.R
+++ baseline.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(janitor)
 library(finalfit)
 library(zoo)
+library(slider)
 
 theme_set(theme_bw())
 
@@ -35,8 +36,61 @@ missing_data <- missing_glimpse(data)
 ### Plot of daily attends
 plot_df <- data |>
   count(check_in_date, weekday_name)|>
-  arrange(check_in_date, weekday_name) |> 
+  arrange(check_in_date) |>
+  # Group by weekday to calculate the forecast for each day independently
+  group_by(weekday_name) |>
+  # Create the forecast column
   mutate(
-    rolling_12wk_avg = zoo::rollmean(n, k = 12, fill = NA, align = "right")
+    baseline_forecast = slide_dbl(
+      .x = lag(n),       
+      .f = ~mean(.x, na.rm = TRUE), 
+      .before = 11,      # The window includes the current (lagged) value plus the 11 before it
+      .complete = FALSE # Allow calculations even if there are fewer than 12 previous values
+    ),
+    baseline_forecast = round(baseline_forecast),
+    diff = baseline_forecast - n
+  )|>
+  rename(actual_values = n) |>
+  ungroup() |>
+  filter(check_in_date >= ymd("2025-09-01"))
+
+plot_df2 <- plot_df |>
+  pivot_longer(
+    cols = c(actual_values, baseline_forecast),
+    names_to = "type",
+    values_to = "value"
   )
 
+
+plot_df2 |>
+  ggplot(aes(x = check_in_date, y = value, color = type)) +
+  geom_line() +
+  geom_point(size = 1) +
+  scale_color_manual(values = c(
+    "actual_values" = "black",
+    "baseline_forecast" = "deeppink3"
+  )) +
+  labs(color = NULL,
+       x = NULL,
+       y = "Attendances",
+       title = "ED Attendances vs Baseline Values")
+
+plot_df |>
+  mutate(sign = ifelse(diff >= 0, "Positive", "Negative")) |>
+  ggplot(aes(x = check_in_date, y = diff, fill = sign)) +
+  geom_col() +
+  scale_fill_manual(
+    values = c(
+      "Positive" = "darkcyan", # Earthy, desaturated green
+      "Negative" = "plum4"  # Muted plum/violet
+    )) +
+  labs(fill = NULL,
+       x = NULL,
+       y = "Prediction Error",
+       title = "Baseline Forecast Prediction Error") +
+  theme(legend.position = "none")
+
+
+## All error distributions
+
+    
